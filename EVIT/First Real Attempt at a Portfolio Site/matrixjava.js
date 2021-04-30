b9478e0989a43d7b7ad22b9a467f6f4e206003fe
AKolar500 <76533984+AKolar500@users.noreply.github.com>
2021-04-30 16:37:45 -0700

    EVIT-Projects

---
+++ EVIT/First Real Attempt at a Portfolio Site/matrixjava.js
@@ -0,0 +1,10 @@
+<!--
+function layerOpen(layerID) {
+document.getElementById('layer' + layerID).style.display = "block";
+document.getElementById('clasp' + layerID).innerHTML="<a href=\"javascript:layerClose('" + layerID + "');\">Close Layer " + layerID + "...</a>";
+}
+function layerClose(layerID) {
+document.getElementById('layer' + layerID).style.display = "none";
+document.getElementById('clasp' + layerID).innerHTML="<a href=\"javascript:layerOpen('" + layerID + "');\">Open Layer " + layerID + "...</a>";
+}
+// -->
